5e7a19f6339f7d1ae5547e040a4c1b93044d5fd8
jplaulau14 <prod.patricklaurel@gmail.com>
2023-03-30 12:44:40 +0800

    parameterized flow

---
+++ week2/parameterized_flow.py
@@ -0,0 +1,68 @@
+from pathlib import Path 
+import pandas as pd 
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+@task(log_prints=True, retries=5)
+def extract(dataset_url: str) -> pd.DataFrame:
+    """
+    Extracts the dataset from the URL
+    """
+    df = pd.read_parquet(dataset_url)
+    return df
+
+@task(log_prints=True)
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transforms the dataset
+    """
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+    return df
+
+@task(log_prints=True)
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """
+    Writes the transformed dataset to a local file
+    """
+    path = Path(f"data/{color}_tripdata/{dataset_file}.parquet")
+    path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_parquet(path)
+    return path
+
+@task(log_prints=True)
+def write_gcs(file_path: Path) -> None:
+    """
+    Writes the transformed dataset to GCS
+    """
+    gcp_cloud_storage_bucket_block = GcsBucket.load("week2-web-to-gcs-connector")
+    gcp_cloud_storage_bucket_block.upload_from_path(
+        from_path=f"{file_path}",
+        to_path=file_path
+    )
+    return
+
+@flow()
+def etl_web_to_gcs(year: int, month: int, color: str) -> None:
+    """
+    The main ETL function
+    """
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}.parquet"
+
+    df = extract(dataset_url)
+    df_transformed = transform(df)
+    path = write_local(df_transformed, color, dataset_file)
+    print(path)
+    write_gcs(path)
+
+@flow()
+def etl_parent_flow(months: list[int], year: int, color: str = "yellow"):
+    for month in months:
+        etl_web_to_gcs(year=year, month=month, color=color)
+
+if __name__ == "__main__":
+    color = "yellow"
+    year = 2021
+    months = [1, 2, 3]
+    etl_parent_flow(months, year, color)
