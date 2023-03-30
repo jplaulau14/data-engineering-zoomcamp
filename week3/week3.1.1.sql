8e9df2203d36eca72151d36a1eb4ab558b557e41
jplaulau14 <prod.patricklaurel@gmail.com>
2023-03-30 21:15:38 +0800

    created non-partitioned and partitioned table

--- week3/week3.1.1.sql
+++ week3/week3.1.1.sql
@@ -1,5 +1,15 @@
+-- Creates an external table on top of the data in GCS
 CREATE OR REPLACE EXTERNAL TABLE `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.external_yellow_tripdata`
 OPTIONS (
     format = 'PARQUET',
     uris = ['gs://week2-prefect-de-zoomcamp/data/yellow_tripdata/yellow_tripdata_2021-*.parquet']
 );
+
+-- Check the external table
+SELECT * FROM `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.external_yellow_tripdata` LIMIT 10;
+
+-- Create a non-partitioned table from the external table
+CREATE OR REPLACE TABLE `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.yellow_tripdata_non_partitioned` AS SELECT * FROM `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.external_yellow_tripdata`;
+
+-- Create a partitioned table from external table
+CREATE OR REPLACE TABLE `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.yellow_tripdata_partitioned` PARTITION BY DATE(tpep_pickup_datetime) AS SELECT * FROM `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.external_yellow_tripdata`;
