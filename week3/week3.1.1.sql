7d4835ec50bf706081bff3f408998f8ab2079854
jplaulau14 <prod.patricklaurel@gmail.com>
2023-03-30 21:20:29 +0800

    compared query efficiency

--- week3/week3.1.1.sql
+++ week3/week3.1.1.sql
@@ -13,3 +13,11 @@ CREATE OR REPLACE TABLE `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp
 
 -- Create a partitioned table from external table
 CREATE OR REPLACE TABLE `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.yellow_tripdata_partitioned` PARTITION BY DATE(tpep_pickup_datetime) AS SELECT * FROM `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.external_yellow_tripdata`;
+
+-- Compare the performance when querying the non-partitioned table and the partitioned table
+SELECT DISTINCT(PULocationID) FROM `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.yellow_tripdata_non_partitioned` WHERE DATE(tpep_pickup_datetime) BETWEEN '2021-01-01' AND '2021-06-30';
+
+SELECT DISTINCT(PULocationID) FROM `yellow-taxi-trips-data-382114.week2_prefect_de_zoomcamp.yellow_tripdata_partitioned` WHERE DATE(tpep_pickup_datetime) BETWEEN '2021-01-01' AND '2021-06-30';
+
+-- Look into the partitions
+SELECT table_name, partition_id, total_rows FROM `week2_prefect_de_zoomcamp.INFORMATION_SCHEMA_PARTITIONS` WHERE table_name = 'yellow_tripdata_partitioned' ORDER BY total_rows DESC;
